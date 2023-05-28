e8aff596aaddcbaa6fed32bc9aa58a8581975afa
Ghos7h <pras6enjit@gmail.com>
2023-05-28 19:16:37 +0530

    first commit

---
+++ traverseLinkedList.js
@@ -0,0 +1,25 @@
+class Node {
+    constructor(val){
+        this.val = val;
+        this.next = null
+    }
+}
+
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+
+a.next = b; b.next = c; c.next = d;
+
+const traverseLinkedList = (head) => {
+    if(!head.next) {
+        console.log('tail = ', head.val);
+        return;
+    }
+    console.log('current = ', head.val)
+    return traverseLinkedList(head.next);
+}
+
+
+traverseLinkedList(a);
