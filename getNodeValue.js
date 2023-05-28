9a68e8885b227aabbb8ed1e20a56d167aa98c59d
Ghos7h <pras6enjit@gmail.com>
2023-05-28 20:52:19 +0530

    next version of getting a value by index provided by function invoker

--- getNodeValue.js
+++ getNodeValue.js
@@ -14,13 +14,19 @@ a.next = b;
 b.next = c;
 c.next = d;
 
-const getNodeValue = (head, index, count = 0) => {
-    if (!head) return null;
-    if (index === count) return head.val;
-    count++;
-    return getNodeValue(head.next, index, count)
+// const getNodeValue = (head, index, count = 0) => {
+//     if (!head) return null;
+//     if (index === count) return head.val;
+//     count++;
+//     return getNodeValue(head.next, index, count)
+// }
+
+const getNodeValueV1 = (head, index) => {
+    if(!head) return null;
+    if(index === 0) return head.val;
+    return getNodeValueV1(head.next, index-1);
 }
-console.log("🚀 ~ file: getNodeValue.js:21 ~ getNodeValue ~ getNodeValue(head.next, index, count):", getNodeValue(a, 2))
-console.log("🚀 ~ file: getNodeValue.js:21 ~ getNodeValue ~ getNodeValue(head.next, index, count):", getNodeValue(a, 0))
-console.log("🚀 ~ file: getNodeValue.js:21 ~ getNodeValue ~ getNodeValue(head.next, index, count):", getNodeValue(a, 10))
-console.log("🚀 ~ file: getNodeValue.js:21 ~ getNodeValue ~ getNodeValue(head.next, index, count):", getNodeValue(null, 0))
+console.log("🚀 ~ file: getNodeValue.js:21 ~ getNodeValue ~ getNodeValue(head.next, index, count):", getNodeValueV1(a, 2))
+console.log("🚀 ~ file: getNodeValue.js:21 ~ getNodeValue ~ getNodeValue(head.next, index, count):", getNodeValueV1(a, 0))
+console.log("🚀 ~ file: getNodeValue.js:21 ~ getNodeValue ~ getNodeValue(head.next, index, count):", getNodeValueV1(a, 10))
+console.log("🚀 ~ file: getNodeValue.js:21 ~ getNodeValue ~ getNodeValue(head.next, index, count):", getNodeValueV1(null, 0))
